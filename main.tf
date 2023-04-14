0198075711c43d59792412d0d2e69f1c70564684
Brian McClain <brianmmcclain@gmail.com>
2023-04-14 10:48:48 -0400

    Update provider versions

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.17.0"
+      version = "2.19.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "2.8.0"
+      version = "2.9.0"
     }
   }
 }
