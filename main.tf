faa68ba4867c90962526d0404983c9493dcce144
Brian McClain <brianmmcclain@gmail.com>
2023-10-27 10:12:50 -0400

    Bump provider versions

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.19.0"
+      version = "2.23.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = "2.9.0"
+      version = "2.11.0"
     }
   }
 }
